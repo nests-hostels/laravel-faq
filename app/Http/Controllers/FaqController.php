173fba830a5d68f724b5920201afc057e428e27d
Artur Mamedov <arturmamedov1993@gmail.com>
2025-02-18 11:15:05 +0100

    FAQ 1 - Controllers and routes defined

---
+++ app/Http/Controllers/FaqController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Faq;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\StoreFaqRequest;
+use App\Http\Requests\UpdateFaqRequest;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class FaqController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $faqs = Faq::all();
+
+        dd($faqs);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorefaqRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(faq $faq)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(faq $faq): Response
+    {
+        return Inertia::render('Faq/Edit', [
+            'faq' => $faq,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFaqRequest $request, faq $faq)
+    {
+        $faq->fill($request->validated());
+
+        $faq->save();
+
+        return Redirect::route('faq.edit', $faq->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(faq $faq)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FaqController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -26,5 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Route::middleware('auth')->group(function () {
+Route::resource('faq', FaqController::class);
+// });
+
+
 
 require __DIR__ . '/auth.php';
