8eb3f069f1bd4b9873480ccee40e34beed261461
Artur Mamedov <arturmamedov1993@gmail.com>
2025-04-08 11:22:43 +0100

    Revert "UserFactory - Updated with my data user and password"
    This reverts commit 8928489542b03f71982b310bb10d06f13cdc0ce7.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'Artur',
-            'email' => 'arturmamedov1993@gmail.com',
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('dfxH6?Af7Dw=3:Y'), // static::$password ??= Hash::make('password')
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +37,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn(array $attributes) => [
+        return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
